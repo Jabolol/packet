e7f69877acbf3ef76ccb7e3425c428618e0b14b4
xRozzo <david.salvatella.gelpi@gmail.com>
2023-10-07 09:18:43 +0200

    feat: add users funciton in teleoperator facet

--- blockend/src/facets/TeleoperatorFacet.sol
+++ blockend/src/facets/TeleoperatorFacet.sol
@@ -2,17 +2,28 @@
 pragma solidity 0.8.19;
 
 import {Modifiers} from "../libraries/AppStorage.sol";
+import {STypes} from "../libraries/DataTypes.sol";
 
+import {Errors} from "../libraries/Errors.sol";
 
 contract TelopertorFacet is Modifiers {
+    function setUsers(address[] calldata users, bytes4 teleoperatorSelector)
+        external
+        onlyValidTeleoperator(msg.sender)
+    {
+        if (s.teleoperators[teleoperatorSelector].adminAddress != msg.sender) {
+            revert Errors.NotAdmin();
+        }
 
-    // NEED THE MODIFIER ONLY VALID TELEOPERATOR
-
-    // setter to add a batch of users
-
-    //" set a batch of users but pass the array of the type...
-    // pass user addresses and create all the users
-    function setUsers(address[] calldata users) external onlyValidTeleoperator(msg.sender) {
-       // valid operartors
+        //  ! need the dataEscrowed put by the teleoperator or no???
+        for (uint64 i = 0; i < users.length; i++) {
+            s.teleoperatorUser[msg.sender][i] = STypes.TeleoperatorUser({ // this can be done with selecotr
+                addr: users[i],
+                dataEscrowed: 0,
+                isFrozen: false
+            });
+        }
     }
+
+    // setter function to set the data scrowed??
 }
