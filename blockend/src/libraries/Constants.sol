091a893a581748b66391fecd3b8713f7e4d6c9c3
xRozzo <david.salvatella.gelpi@gmail.com>
2023-10-07 17:54:56 +0200

    feat: add external accounting with dataToken contract

--- blockend/src/libraries/Constants.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-pragma solidity 0.8.21;
-
-library Constants {
-    address internal constant USDC_POLYGON = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);  // polygon (mainnet)
-    address internal constant USDC_MAINNET = address(0x2); // mainnet
-    address internal constant USDC_SEPOLIa = address(0x2); // rinkeby
-
-}
