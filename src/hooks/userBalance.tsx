74d5066fd07450c8cd6805bbac7d3ebfeb86f32b
Javier R <javier.rios-urbano@epitech.eu>
2023-10-08 06:04:43 +0200

    fix: dynamic `balance`

--- src/hooks/userBalance.tsx
+++ src/hooks/userBalance.tsx
@@ -1,13 +1,14 @@
 import { useContractRead } from "wagmi";
 import { BOSExampleAbi } from "../utils/wagmi-constants";
+import useAddress from "./useAddress";
 
 export const useUserBalance = () => {
+  const { address } = useAddress();
   const { data, isError, isLoading } = useContractRead({
     address: `0x8925b3d8Cb999fdb8850E715588Ba22028E1C831`,
     abi: BOSExampleAbi,
     functionName: "userDataBalances",
-    // TODO(jabolo): This should be the user's address
-    args: [`0xE210058ca7710330ad56Cf38c69F516b8D325701`],
+    args: [address],
   });
   return { data, isError, isLoading };
 };
